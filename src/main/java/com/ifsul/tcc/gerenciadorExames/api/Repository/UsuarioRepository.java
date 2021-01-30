5a1132e8f10d2531df303673b754d9c7f58559ba
mariohpr <mariopereira398@gmail.com>
2021-01-30 12:32:52 -0300

    teste ovo

--- src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/UsuarioRepository.java
+++ src/main/java/com/ifsul/tcc/gerenciadorExames/api/Repository/UsuarioRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
     Optional<Usuario> findByEmail(String email);
 }
